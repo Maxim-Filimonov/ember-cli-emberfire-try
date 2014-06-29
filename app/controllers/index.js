70d85b86b24c2e5cb3621dcffaa281f7231aead3
Maxim Filimonov <tpaktopsp@gmail.com>
2014-06-29 17:44:38 +1000

    add inputs to custommize data

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  init: function() {
+    this.set('post',  Ember.Object.create());
+  },
   actions: {
     createPost: function() {
-      var newPost = this.store.createRecord('post', {title: 'bla', name: 'bla'});
+      var newPost = this.store.createRecord('post', {
+        title: this.get('post.title'),
+        name: this.get('post.name')
+      });
       var result = newPost.save();
       console.log("POST created", result);
     }
